c4c10565af9b744d198d48862a5a66ddef42e807
Lucas Engel <lucasengel@gmail.com>
2020-09-22 22:05:02 -0400

    Focus and Filled input styling

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,20 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const StyledInput = styled.div`
+interface StyledInputProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const StyledInput = styled.div<StyledInputProps>`
   align-items: center;
   background: #232129;
-  border-radius: 10px;
   border: 2px solid #232129;
+  border-radius: 10px;
+  color: #666360;
   display: flex;
   padding: 16px;
   width: 100%;
+
+  ${props =>
+    props.isFocused === true &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled === true &&
+    css`
+      color: #ff9000;
+    `}
 
   & + & {
     margin-top: 8px;
   }
 
   svg {
-    color: #666360;
     margin-right: 16px;
   }
 
