2d4d8a9ef8025b227b87688558f657667afee93b
Lucas Engel <lucasengel@gmail.com>
2020-10-21 22:24:11 -0400

    Dashboard screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,111 @@
-import React from 'react';
-import { Container, Header, HeaderContent, NextAppointment, Profile, Schedule, Calendar, Content } from './styles'
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { isAfter, isBefore, isToday } from 'date-fns';
+import { format } from 'date-fns/esm';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css'
+import { FiClock, FiPower } from 'react-icons/fi';
+
+import { Container, Header, HeaderContent, NextAppointment, Profile, Schedule, Calendar, Content, Section, Appointment } from './styles'
 
 import logoImg from '../../assets/logo.svg'
-import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+interface MonthAvailabilityItem {
+  day: number;
+  available: boolean
+}
+
+interface Appointment {
+  id: string;
+  date: string;
+  user: {
+    id: string;
+    name: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
   const { user, logOut } = useAuth()
+
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date())
+  const [currentMonth, setCurrentMonth] = useState<Date>(new Date())
+  const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([])
+  const [appointments, setAppointments] = useState<Appointment[]>([])
+
+  const handleDateChange = useCallback(
+    (day: Date, modifiers: DayModifiers): void => {
+      if (modifiers.available && !modifiers.disabled) setSelectedDate(day)
+    },
+    [],
+  )
+
+  const handleMonthChange = useCallback(
+    (month: Date) => setCurrentMonth(month),
+    [],
+  )
+
+  const disabledDays = useMemo(() => {
+    const year = currentMonth.getFullYear()
+    const month = currentMonth.getMonth()
+
+    const dates = monthAvailability
+      .filter(day => !day.available)
+      .map(day => new Date(year, month, day.day))
+
+    return dates
+  }, [currentMonth, monthAvailability])
+
+  const selectedWeekdayAsText = useMemo(() => format(selectedDate, 'EEEE'),
+    [selectedDate]
+  )
+
+  const selectedDayAsText = useMemo(() => format(selectedDate, 'LLLL do'),
+    [selectedDate]
+  )
+
+  useEffect(() => {
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1
+        }
+      })
+      .then(response => {
+        setMonthAvailability(response.data)
+      })
+  }, [currentMonth, user.id])
+
+  useEffect(() => {
+    api
+      .get('/appointments/schedule', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        }
+      })
+      .then(response => {
+        setAppointments(response.data)
+      })
+
+  }, [selectedDate])
+
+  const morningAppointments = useMemo(() => appointments.filter(
+    appointment => new Date(appointment.date).getHours() < 13
+  ), [appointments])
+
+  const afternoonAppointments = useMemo(() => appointments.filter(
+    appointment => new Date(appointment.date).getHours() >= 13
+  ), [appointments])
+
+  const nextAppointment = useMemo(() => {
+    return appointments
+      .sort((a, b) => new Date(a.date) > new Date(b.date) ? 1 : -1)
+      .find(appointment => isAfter(new Date(appointment.date), new Date()))
+  }, [appointments])
 
   return (
     <Container>
@@ -27,22 +126,69 @@ const Dashboard: React.FC = () => {
         <Schedule>
           <h1>Scheduled appointments</h1>
           <p>
-            <span>Today</span>
-            <span>Day 6</span>
-            <span>Monday</span>
+            {isToday(selectedDate) && <span>Today</span>}
+            <span>{selectedWeekdayAsText}</span>
+            <span>{selectedDayAsText}</span>
           </p>
 
-          <NextAppointment>
+          {isToday(selectedDate) && nextAppointment && <NextAppointment>
             <strong>Next appointment</strong>
             <div>
-              <img src="http://localhost:3333/files/57e9068169ea9934d628-profile-sq.jpg" alt="Lucas Engel" />
-              <strong>Lucas Engel</strong>
-              <span><FiClock /> 08:00</span>
+              <img src={nextAppointment.user.avatar_url} alt={nextAppointment.user.name} />
+              <strong>{nextAppointment.user.name}</strong>
+              <span><FiClock /> {format(new Date(nextAppointment.date), 'hhmma')}</span>
             </div>
-          </NextAppointment>
-        </Schedule>
-        <Calendar>
+          </NextAppointment>}
 
+          <Section>
+            <strong>Morning</strong>
+            {morningAppointments.length === 0 && <p>No appointments.</p>}
+            {morningAppointments.sort((a, b) => new Date(a.date) > new Date(b.date) ? 1 : -1).map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {format(new Date(appointment.date), 'hh:mma')}
+                </span>
+
+                <div>
+                  <img src={appointment.user.avatar_url} alt={appointment.user.name} />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+            {afternoonAppointments.length === 0 && <p>No appointments.</p>}
+            {afternoonAppointments.sort((a, b) => new Date(a.date) > new Date(b.date) ? 1 : -1).map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {format(new Date(appointment.date), 'hh:mma')}
+                </span>
+
+                <div>
+                  <img src={appointment.user.avatar_url} alt={appointment.user.name} />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
+          </Section>
+        </Schedule>
+
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['S', 'M', 'T', 'W', 'T', 'F', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] }
+            }}
+            onDayClick={handleDateChange}
+            onMonthChange={handleMonthChange}
+            selectedDays={selectedDate}
+          />
         </Calendar>
       </Content>
     </Container>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,7 @@
+import { shade } from "polished";
 import styled from "styled-components";
+import ArrowLeftIcon from '../../assets/ArrowLeftIcon.svg';
+import ArrowRightIcon from '../../assets/ArrowRightIcon.svg';
 
 export const Container = styled.div``
 
@@ -61,6 +64,7 @@ export const Content = styled.main`
   margin: 64px auto;
   display: flex;
 `
+
 export const Schedule = styled.div`
   flex: 1;
   margin-right: 160px;
@@ -144,6 +148,148 @@ export const NextAppointment = styled.div`
   }
 `
 
+export const Section = styled.section`
+  margin-top: 48px;
+
+  > strong {
+    border-bottom: 1px solid #3e3b47;
+    color: #999591;
+    display: block;
+    font-size: 20px;
+    line-height: 26px;
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+  }
+
+  p {
+    color: #999591;
+  }
+`
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    color: #f4ede8;
+    flex-basis: 100px;
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    align-items: center;
+    background: #3e3b47;
+    border-radius: 10px;
+    display: flex;
+    flex: 1;
+    padding: 16px 24px;
+    margin-left: 24px;
+
+    img {
+      border-radius: 50%;
+      height: 56px;
+      width: 56px;
+    }
+
+    strong{
+      font-size: 20px;
+      margin-left: 24px;
+      color: #fff;
+    }
+  }
+`
+
 export const Calendar = styled.aside`
   width: 380px;
+
+  .DayPicker {
+    border-radius: 10px;
+    font-size: 16px;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+    background: #3e3b47;
+    border-radius: 10px;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-NavButton {
+    color: #999591 !important;
+  }
+
+  .DayPicker-NavButton--prev {
+    background: url(${ArrowLeftIcon}) no-repeat center;
+    right: auto;
+    left: 16px;
+    margin-right: 0;
+  }
+
+  .DayPicker-NavButton--next {
+    background: url(${ArrowRightIcon}) no-repeat center;
+  }
+
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px 0 0 0;
+    padding: 16px;
+    background-color: #28262e;
+    border-radius: 0 0 10px 10px;
+  }
+
+  .DayPicker-Caption {
+    margin-bottom: 1em;
+    padding: 0 1em;
+    color: #f4ede8;
+
+    > div {
+      text-align: center;
+    }
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: #3e3b47;
+    border-radius: 10px;
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, '#3e3b47')};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+    color: #fff;
+  }
+
+  .DayPicker-Day--disabled {
+    color: #666360;
+    background: transparent !important;
+  }
+
+  .DayPicker-Day--selected {
+    background: #ff9000 !important;
+    border-radius: 10px;
+    color: #232129 !important;
+  }
 `
